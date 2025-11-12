6e62d2d436d32daa3272e4a1615c3df92641dd4d
thuanquang <ghoulbulldog@gmail.com>
2025-11-12 15:42:18 +0700

    progress

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,14 @@
+import Navbar from "@/app/components/navbar"
+
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <main className="font-work-sans">
-            
-            {children}
-        </main>
+        <html>
+            <body>
+                <main className="font-work-sans">
+                    <Navbar />
+                    {children}
+                </main>
+            </body>
+        </html>
     )
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <>
-    HOME
+    <h1 className='text-3xl font-bold'>HOME</h1>
     </>
   )
 }
