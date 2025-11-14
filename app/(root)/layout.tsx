c47685a854f114485a87984d144829eb47ae6dfd
thuanquang <ghoulbulldog@gmail.com>
2025-11-14 15:59:40 +0700

    Update dependencies and enhance layout with custom fonts; modify navbar and page content for improved design

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,57 @@
 import Navbar from "@/app/components/navbar"
+import localFont from "next/font/local"
+import { Metadata } from "next"
+import '../../globals.css'
 
+const workSans = localFont({
+    src: [
+        {
+            path: '../fonts/WorkSans-Black.ttf',
+            weight: '900',
+            style: 'normal'
+        }, {
+            path: '../fonts/WorkSans-ExtraBold.ttf',
+            weight: '800',
+            style: 'normal'
+        }, {
+            path: '../fonts/WorkSans-Bold.ttf',
+            weight: '700',
+            style: 'normal'
+        }, {
+            path: '../fonts/WorkSans-SemiBold.ttf',
+            weight: '600',
+            style: 'normal'
+        }, {
+            path: '../fonts/WorkSans-Medium.ttf',
+            weight: '500',
+            style: 'normal'
+        }, {
+            path: '../fonts/WorkSans-Regular.ttf',
+            weight: '400',
+            style: 'normal'
+        }, {
+            path: '../fonts/WorkSans-Thin.ttf',
+            weight: '200',
+            style: 'normal'
+        }, {
+            path: '../fonts/WorkSans-ExtraLight.ttf',
+            weight: '100',
+            style: 'normal'
+        }
+    ],
+    variable: '--font-work-sans'
+})
+
+export const metadata: Metadata = {
+    title: 'YC Directory',
+    description: 'Find the best startups for your business'
+}
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html>
-            <body>
-                <main className="font-work-sans">
-                    <Navbar />
+            <body className={`${workSans.variable}`}>
+                <main>
+                    <Navbar/>
                     {children}
                 </main>
             </body>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 const page = () => {
   return (
     <>
-    <h1 className='text-3xl font-bold'>HOME</h1>
+    <section className='pink_container pattern'>
+      <h1 className='heading'>Pitch Your Startup, <br/> Connect With Entrepreneurs</h1>
+      <p className='font-medium text-[20px] text-white max-w-3xl text-center break-words'>Join a community of founders and investors to pitch your startup, get feedback, and connect with like-minded individuals.</p>
+    </section>
+    
     </>
   )
 }
